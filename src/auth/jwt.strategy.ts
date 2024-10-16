54c7102508de62d6fb251cc4b3999e400c734968
Amit3817 <amitverma3817@gmail.com>
2024-10-16 23:02:38 +0530

    Fixed userid bug

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UsersService } from '../users/users.service'; // Your UsersService
-import { JwtPayload } from './jwt-payload.interface'; // Create this interface
+import { UsersService } from '../users/users.service';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,6 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return await this.usersService.getUserById(payload.sub); // Return the user based on the token payload
+    const user = await this.usersService.getUserById(payload.sub);
+    return user;
   }
 }
